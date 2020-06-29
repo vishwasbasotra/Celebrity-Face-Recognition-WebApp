c00ef858334dd10d434e78366336b7647323d139
Vishwas Basotra <brigu.sharma.9@gmail.com>
2020-06-29 12:32:55 +0530

    app

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import json
+import util
+from flask import Flask, request, jsonify, url_for, redirect, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    result = jsonify(util.classify_image(image_data))
+
+    result.headers.add('Access-Control-Allow-Origin', '*')
+
+    return result
+
+
+if __name__ == "__main__":
+    print('Starting Python Flask Server For Sports Celebrity Image Classification!!!')
+    util.load_artifacts()
+    app.run(debug=True)
